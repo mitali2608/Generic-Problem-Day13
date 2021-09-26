1fc3d24ee85c110393795d8c5c827e0efc643940
mitali2608 <mitalibawankule21@gmail.com>
2021-09-27 01:50:16 +0530

    Deleting contact from Address Book

---
+++ AddressBookProblem/AddressBookProblem/UC4DeleteContacts.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Address_Book
+{
+    class UC4DeleteContacts
+    {
+        public static void RemoveC1()
+        {
+            Console.WriteLine("Enter the first name you want to remove.");
+            string firstName = Console.ReadLine();
+            Datainput C1 = Program.People.FirstOrDefault(x => x.FirstName.ToLower() == firstName.ToLower());
+
+            if (C1 == null)
+            {
+                Console.WriteLine("That C1 could not be found. Press any key to continue. ");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("Are you sure you want to remove C1 from this Address Book? (Y/N)");
+            Program.PrintC1(C1);
+
+            if (Console.ReadKey().Key == ConsoleKey.Y)
+            {
+                Program.People.Remove(C1);
+                Console.WriteLine("C1 removed. Press any key to continue");
+                Console.ReadKey();
+            }
+        }
+    }
+}
