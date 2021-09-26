57459a75b9f2ae78ea497568bb23e61dc8536e5c
mitali2608 <mitalibawankule21@gmail.com>
2021-09-27 01:39:04 +0530

    Using Contact class to add contact in address book

---
+++ AddressBookProblem/AddressBookProblem/UC2AddContacts.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookProblem
+{
+    class UC2AddContacts
+    {
+        public static void AddPerson()
+        {
+            Datainput person = new Datainput();
+
+            Console.WriteLine("First Name -");
+            person.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Last Name -");
+            person.LastName = Console.ReadLine();
+
+            Console.WriteLine("Address -");
+            person.Address = Console.ReadLine();
+
+            Console.WriteLine("City -");
+            person.City = Console.ReadLine();
+
+            Console.WriteLine("State -");
+            person.State = Console.ReadLine();
+
+            Console.WriteLine("Zip -");
+            person.Zip = Console.ReadLine();
+
+            Console.WriteLine("Phone Number -");
+            person.PhoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Email -");
+            person.Email = Console.ReadLine();
+
+            Program.People.Add(person);
+        }
+    }
+}
