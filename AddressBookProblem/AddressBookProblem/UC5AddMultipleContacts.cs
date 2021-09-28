6b98c7f2aba6a55b423b86628915617ffe83dd18
Mitali Bawankule <88092878+mitali2608@users.noreply.github.com>
2021-09-28 22:39:01 +0530

    Add files via upload

---
+++ AddressBookProblem/AddressBookProblem/UC5AddMultipleContacts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookProblem
+{
+        class UC5AddMultipleContacts
+        {
+            public static void addMultiple()
+            {
+                int num;
+                Console.WriteLine("Enter the number of contacts you want to create-");
+                num = Convert.ToInt32(Console.ReadLine());
+                int i = 0;
+                while (i < num)
+                {
+                UC2AddContacts.AddPerson();
+                    Console.WriteLine("---------------------------");
+                    i++;
+                }
+            }
+        }
+    }
+
